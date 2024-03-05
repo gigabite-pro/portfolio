9a2abb60c0a29da49c7caa2faadf173b95739662
Vaibhav Sharma <rajmachawal19@gmail.com>
2024-03-05 00:00:17 -0800

    phone redirect bug

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,25 @@ import CustomLoader from "./components/CustomLoader";
 import HomeCanvas from "./components/HomeCanvas";
 import { Canvas } from "@react-three/fiber";
 import Tips from "./components/Tips";
-import { Link } from "react-router-dom";
 
 const Home = () => {
+    if (window.innerWidth < 768) {
+        document.body.innerHTML = "";
+        document.body.style.backgroundColor = "black";
+        document.body.style.width = "100vw";
+        document.body.style.height = "100vh";
+        document.body.style.color = "white";
+        document.body.style.display = "grid";
+        document.body.style.placeItems = "center";
+        document.body.style.fontSize = "5vw";
+        document.body.style.fontWeight = "bold";
+        document.body.innerHTML =
+            "<p>Sorry! This website is currently not available on mobile devices. <br> Please visit on a desktop or laptop. Redirecting to old website...</p>";
+        setTimeout(() => {
+            window.location.href = "https://vaibhav-sharma.me";
+        }, 7000);
+    }
+
     useEffect(() => {
         document.querySelector(".tip-overlay").style.display = "flex";
         setTimeout(() => {
